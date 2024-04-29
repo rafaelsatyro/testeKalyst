351d2ca5097bf518a3e7188c670babc21610cdde
rafaelsatyro <rafael.satyro@hotmail.com>
2024-04-29 06:09:16 -0300

    inserido condicional ternário

--- src/app/components/Checkout.tsx
+++ src/app/components/Checkout.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import './Checkout.css';
 import leftarrow from '../../../public/seta.png';
 import circle from '../../../public/bola.png'
 import boleto from '../../../public/boleto.png'
 import pix from '../../../public/pix.png';
 
+
 const Checkout: React.FC = () => {
+   const [paymentMethod, setPaymentMethod]  = useState('card');
   return (
     <div className='main-container'>
         <div className='left-side-container'>
@@ -63,32 +66,67 @@ const Checkout: React.FC = () => {
                 <p className='tenth-line-side-contact'>Contact</p>
             </div>
         </div>
-        <div className='right-side-container'>
+
+        <div className='right-side-container'>    
            <div className='first-line-right-side'>
-                <button className='first-line-right-side-pix'><img src={pix.src} alt="pix" className='first-line-right-side-pix-icon'/>  Pix</button>
-                <button className='first-line-right-side-boleto'><img src={boleto.src} alt="boleto" className='first-line-right-side-boleto-icon' />Boleto</button>
-           </div>
+           {paymentMethod === 'card' ? (
+                <>
+                    <button className='first-line-right-side-pix' onClick={() => setPaymentMethod('pix')}>
+                        <img src={pix.src} alt="pix" className='first-line-right-side-pix-icon' /> Pix
+                    </button>
+                    <button className='first-line-right-side-boleto' onClick={() => setPaymentMethod('boleto')}>
+                        <img src={boleto.src} alt="boleto" className='first-line-right-side-boleto-icon' />Boleto
+                    </button>
+                </>
+            ) : paymentMethod === 'pix' ? (
+                <>
+                    <button className='first-line-right-side-pix' onClick={() => setPaymentMethod('card')}>
+                        <img src={pix.src} alt="pix" className='first-line-right-side-pix-icon' /> Cartão de Crédito
+                    </button>
+                    <button className='first-line-right-side-boleto' onClick={() => setPaymentMethod('boleto')}>
+                        <img src={boleto.src} alt="boleto" className='first-line-right-side-boleto-icon' />Boleto
+                    </button>
+                </>
+            ) : (
+                <>
+                    <button className='first-line-right-side-pix' onClick={() => setPaymentMethod('pix')}>
+                        <img src={pix.src} alt="pix" className='first-line-right-side-pix-icon' /> Pix
+                    </button>
+                    <button className='first-line-right-side-boleto' onClick={() => setPaymentMethod('card')}>
+                        <img src={boleto.src} alt="boleto" className='first-line-right-side-boleto-icon' /> Cartão de Crédito
+                    </button>
+                </>
+            )}
+        </div>
+           {paymentMethod === 'pix' ?
+            <p>Pix</p>
+            :
+            (paymentMethod === 'boleto') ?
+
+            <p>Boleto</p>
+            :
+            <>
             <div className='second-line-right-side'>
-                <hr className='second-line-right-side-hr'/>
+                <hr className='second-line-right-side-hr' />
                 <p className='second-line-right-side-pay-with-card'>Or pay with card</p>
-                <hr className='second-line-right-side-hr'/>
+                <hr className='second-line-right-side-hr' />
             </div>
             <div className='third-line-right-side'>
                 <p className='third-line-right-side-contact-title'>Contact information</p>
-                <input type="text" className='third-line-right-side-email-input' placeholder='email.@example.com'/>
-                <input type="text" className='third-line-right-side-phone-input' placeholder='(00) 0 0000-0000'/>
+                <input type="text" className='third-line-right-side-email-input' placeholder='email.@example.com' />
+                <input type="text" className='third-line-right-side-phone-input' placeholder='(00) 0 0000-0000' />
             </div>
             <div className='fourth-line-right-side'>
                 <p className='fourth-line-right-side-contact-title'>Card information</p>
-                <input type="text" className='fourth-line-right-side-card-input' placeholder='1234 1234 1234 1234'/>
+                <input type="text" className='fourth-line-right-side-card-input' placeholder='1234 1234 1234 1234' />
                 <div className='fourth-line-right-side-inputs-row'>
-                <input type="text" className='fourth-line-right-side-month-and-year-input' placeholder='MM / YY'/>
-                <input type="text" className='fourth-line-right-side-cvc-input' placeholder='CVC'/>
+                    <input type="text" className='fourth-line-right-side-month-and-year-input' placeholder='MM / YY' />
+                    <input type="text" className='fourth-line-right-side-cvc-input' placeholder='CVC' />
                 </div>
             </div>
             <div className='fifth-line-right-side'>
                 <p className='fifth-line-right-side-contact-title'>Name on card</p>
-                <input type="text" className='fifth-line-right-side-name-on-card-input'/>
+                    <input type="text" className='fifth-line-right-side-name-on-card-input' />
             </div>
             <div className='sixth-line-right-side'>
                 <p className='fifth-line-right-side-contact-title'>Installments</p>
@@ -100,99 +138,15 @@ const Checkout: React.FC = () => {
             </div>
             <div className='seventh-line-right-side'>
                 <button className='seventh-line-right-side-button'>Pagar</button>
-            </div>
-            
-
+                </div>
+            </>
+            }
         </div>
     </div>
-     /*
-    <div className="flex flex-col md:flex-row h-screen w-full bg-gray-100">
-      Lado Esquerdo - Sumário do Pedido 
-      <div className="flex-1 flex flex-col justify-between p-8 bg-white">
-        <div className='pl-28 pr-28'>
-            <div className="flex items-center pt-14 gap-2.5">
-               
-                <h1 className="text-4xl font-bold ">Cube.</h1>
-            </div>
-          <div className="flex flex-col flex pt-16 pl-8">
-            <p className="text-3xl font-bold text-slate-300 font-light">Cube.</p>
-            <p className="text-4xl font-bold pt-2">$549,90</p>
-          </div>
-
-          {/* Lista de ofertas 
-          <div className="space-y-2 pt-16">
-            <div className="flex items-center pl-20 ">
-             
-                <div className='flex flex-col pr-52'> 
-                    <p className="text-gray-600">Offer</p>
-                    <p className="text-gray-600">Offer Description</p>
-                </div>
-                <p>RS 19,00</p>
-            </div>
-          </div>
-
-          <div className="space-y-2 pt-16">
-            <div className="flex items-center pl-20 ">
-             
-                <div className='flex flex-col pr-52'> 
-                    <p className="text-gray-600">Offer</p>
-                    <p className="text-gray-600">Offer Description</p>
-                </div>
-                <p>RS 19,00</p>
-            </div>
-          </div>
-
-          <div className='flex justify-between pr-32 pl-20 pt-14 '>
-                <p className="text-gray-600">Subtotal</p>
-                <p className="text-gray-600">R$ 38,00</p>
-          </div>
-          <div className='pr-32 pl-20 pt-4' >
-             <hr />
-          </div>
-
-          <div className='flex justify-between pr-32 pl-20 pt-14 '>
-                <p className="text-gray-600">Sales tax(6.5%)</p>
-                <p className="text-gray-600">R$ 38,00</p>
-          </div>
-          <div className='pr-32 pl-20 pt-4' >
-             <hr />
-          </div>
-
-          <div className='flex justify-between pr-32 pl-20 pt-10 '>
-                <p className="text-2xl font-bold">Total</p>
-                <p className="text-2xl font-bold">R$ 38,00</p>
-          </div>
-        </div>
-    </div>
-*/
-    
-      /* Lado Direito - Detalhes do Pagamento 
-      <div className="flex-1 bg-gray-50 p-8">
-        <div className="flex flex-col pt-14">
-          <button className="mb-6 w-full bg-black text-white py-3 px-4 rounded-xl">
-            Pix
-          </button>
-          <div className="border-t pt-6">
-            <p className="text-lg  mb-4 text-gray-600">Contact information</p>
-            <input className="form-input mb-4 p-2 border w-full rounded-xl" placeholder="email@example.com" />
-            <input className="form-input mb-4 p-2 border w-full rounded-xl" placeholder="(00) 0 0000-0000" />
-            <p className="text-lg  mb-4 text-gray-600">Card information</p>
-            <input className="form-input mb-4 p-2 border w-full rounded-xl" placeholder="1234 1234 1234 1234" />
-            <div className="flex space-x-2">
-              <input className="form-input mb-4 p-2 border flex-1 rounded-xl" placeholder="MM / YY" />
-              <input className="form-input mb-4 p-2 border flex-1 rounded-xl" placeholder="CVC" />
-            </div>
-            <input className="form-input mb-4 p-2 border w-full rounded-xl" placeholder="Name on card" />
-            <select className="form-select mb-6 p-2 border w-full rounded-xl">
-              <option className='text-gray-600'>Installment</option>
-              {/* Opções de parcelamento
-            </select>
-            <button className="w-full bg-black text-white py-3 px-4 rounded-xl">Pagar</button>
-          </div>
-        </div>
-      </div>
-    </div>*/
   );
 }
 
 export default Checkout;
+
+
+
